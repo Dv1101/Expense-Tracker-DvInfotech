5bda1dc11697c8ffd34f44749c213def50ce181c
Dv1101 <dhruvvaria1101@gmail.com>
2024-03-03 15:44:18 +0530

    Add project files.

---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //Category ID FKEY
+        public int CaategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
